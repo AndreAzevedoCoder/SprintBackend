746f755960e9a8727368f010392b655b660768c4
André Santos De Azevedo <54094513841@info.local>
2020-02-17 10:12:29 -0300

    first

---
+++ senai.Filmes.WebApi/senai.Filmes.WebApi/Interfaces/IGeneroRepository.cs
@@ -0,0 +1,21 @@
+﻿using senai.Filmes.WebApi.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace senai.Filmes.WebApi.Interfaces
+{
+    /// <summary>
+    /// Interface responsável pelo repositório Genero
+    /// </summary>
+    interface IGeneroRepository
+    {
+        /// <summary>
+        /// Lista todos os gêneros
+        /// </summary>
+        /// <returns>Retorna uma lista de gêneros</returns>
+        List<GeneroDomain> Listar();
+        void Adicionar(GeneroDomain novoGenero);
+    }
+}
