066beaddd782216adaadb7cbb359e49709349b90
André Santos De Azevedo <54094513841@info.local>
2020-02-21 10:24:48 -0300

    Atualizacao do PostMan

--- Peoples/bd/DML.sql
+++ Peoples/bd/DML.sql
@@ -1,14 +1,24 @@
 USE M_Peoples
 
-INSERT INTO Funcionarios (nome,sobrenome)
-VALUES('Catarina','Strada'),
-('Tadeu','Vitelli');
+INSERT INTO Funcionarios (nome,sobrenome,dataDeNascimento)
+VALUES('Catarina','Strada','18/09/1980'),
+('Tadeu','Vitelli','22/03/1960');
 
-
-CREATE PROCEDURE AdicionarFuncionario @nome VARCHAR(255), @sobrenome VARCHAR(255)
+CREATE PROCEDURE OrdernarNomesASC
 AS
-INSERT INTO Funcionarios(nome,sobrenome)
-VALUES(@nome,@sobrenome)
+SELECT * FROM Funcionarios ORDER BY Nome ASC
+
+
+CREATE PROCEDURE MostrarFuncionarioPorNome @Nome VARCHAR(255)
+AS 
+SELECT * FROM Funcionarios WHERE Nome = @Nome
+
+EXEC MostrarFuncionarioPorNome 'André'
+
+CREATE PROCEDURE AdicionarFuncionario @nome VARCHAR(255), @sobrenome VARCHAR(255), @dataDeNascimento DATE
+AS
+INSERT INTO Funcionarios(nome,sobrenome,DataDeNascimento)
+VALUES(@nome,@sobrenome,@dataDeNascimento) 
 
 
 CREATE PROCEDURE DemitirFuncionario @id INT
@@ -16,14 +26,15 @@ AS
 DELETE FROM Funcionarios WHERE idFuncionario = @id
 
 
-EXEC DemitirFuncionario 4
+EXEC AdicionarFuncionario "Teste", "sem nome", "15/06/2002"
+
+
+ALTER TABLE Funcionarios ADD DataDeNascimento DATE 
+
+
+UPDATE Funcionarios SET DataDeNascimento = '20/09/1973' WHERE idFuncionario = 1;
 
 SELECT * FROM Funcionarios
-
-EXEC AdicionarFuncionario André, Azevedo
-
-
-
 
 
 
